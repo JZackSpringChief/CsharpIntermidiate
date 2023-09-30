dcb5496a897edb44f1a08ef38c889fe6b4c394a1
Zack Spring Chief <jzacksc@gmail.com>
2023-09-29 19:32:17 -0600

    adding to notes Stack and heap

--- Moduleone.cs
+++ Moduleone.cs
@@ -1,3 +1,7 @@
 //Classes and objects
 //creating an object is called "instantiation"
 //charactoristics of an object are called properties
+
+//values and Reference types.
+//Stack is used for static mem allocation, which includes all your value types.
+//Heap is used for dynamic mem allocation, which includes custom objecrs, that might need additional mem during the runtime of your program.
