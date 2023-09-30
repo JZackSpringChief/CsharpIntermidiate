bc97727aab38122b86e277f67ea694b24d89410c
Zack Spring Chief <jzacksc@gmail.com>
2023-09-29 22:16:56 -0600

    module passed

--- Moduleone.cs
+++ Moduleone.cs
@@ -275,3 +275,21 @@ namespace SoloLearn
         	}
     }
 }
+
+
+namespace SoloLearn
+{
+	class Program
+	{
+		class Person
+		{
+			public string Name { get; set; }
+		}
+		static void Main(string[] args)
+		{
+			Person p = new Person();
+			p.Name = "Bob";
+			Console.WriteLine(p.Name);
+		}
+	}
+}
