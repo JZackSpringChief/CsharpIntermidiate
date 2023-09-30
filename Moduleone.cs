1845177724f6339572f9226f4a701b35a7654a63
Zack Spring Chief <jzacksc@gmail.com>
2023-09-29 19:40:51 -0600

    adding notes and example of dot operator

--- Moduleone.cs
+++ Moduleone.cs
@@ -40,3 +40,31 @@ namespace SoloLearn
 		}
 	}
 }
+
+
+//can use the dot "." operator to get info in object
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+	class Program
+	{
+		class Dog
+		{
+			public string name;
+			public int age;
+		}
+		static void Main(string[] args)
+		{
+			Dog bob = new Dog();
+			bob.name = "Bobby";
+			bob.age = 3;
+			
+			Console.WriteLine(bob.age);
+		}
+	}
+}
