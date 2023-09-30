b54dda01ea0bf823dd4b777c991b2f7bc141468d
Zack Spring Chief <jzacksc@gmail.com>
2023-09-29 21:19:06 -0600

    using encapsulation

--- Moduleone.cs
+++ Moduleone.cs
@@ -68,3 +68,79 @@ namespace SoloLearn
 		}
 	}
 }
+
+//encapsulation also meaning information hiding.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+	class BankAccount {
+		private double balance=0;
+		public void Deposit(double n) {
+			balance += n;
+		}
+		public void Withdraw(double n) {
+			balance -= n;
+		}
+		public double GetBalance() {
+			return balance;
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BankAccount b = new BankAccount();
+			b.Deposit(199);
+			b.Withdraw(42);
+			Console.WriteLine(b.GetBalance());
+		}
+	}
+}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int games = Convert.ToInt32(Console.ReadLine()); 
+            int wins = Convert.ToInt32(Console.ReadLine()); 
+
+            //creating the player object
+            Player player1 = new Player();
+            player1.games = games;
+            player1.wins = wins;
+
+            //output
+            player1.GetWinRate();
+        }
+    }
+    
+    class Player
+    {
+        public int games;
+        public int wins;
+        //winrate is private
+        private int winrate;
+
+        //complete the method
+        public void GetWinRate()
+        {
+            	winrate = wins*100/games;
+                Console.WriteLine(winrate);
+        }
+    }
+}
+
