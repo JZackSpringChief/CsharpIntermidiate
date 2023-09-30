3599421827464cd25a24f4aab58a4ea4f5776058
Zack Spring Chief <jzacksc@gmail.com>
2023-09-29 19:37:59 -0600

    bertter example of instatiating and object

--- Moduleone.cs
+++ Moduleone.cs
@@ -15,3 +15,28 @@ class Person
         Console.WriteLine("hi");
     }
 }
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+	class Program
+	{
+		class Person {
+			int age;
+			string name;
+			public void SayHi() {
+				Console.WriteLine("Hi");
+			}
+		}
+		static void Main(string[] args)
+		{
+			Person p1 = new Person();
+			p1.SayHi();
+		}
+	}
+}
