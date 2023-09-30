6001f31dac96f805b531aca7e48098dc786553d9
Zack Spring Chief <jzacksc@gmail.com>
2023-09-29 21:29:34 -0600

    adding and accessing new method

--- Moduleone.cs
+++ Moduleone.cs
@@ -144,3 +144,40 @@ namespace SoloLearn
     }
 }
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a Welcome object with the same name
+            	Welcome welcome = new Welcome();
+            		
+            	
+            
+            
+            welcome.WelcomeMessage();
+            welcome.SuperMessage();
+            
+        }
+    }
+    
+    class Welcome
+    {
+        //complete the class, add WelcomeMessage() method
+        	public void WelcomeMessage()
+        	{
+        		Console.WriteLine("Welcome to OOP");
+        	}
+            public void SuperMessage()
+            {
+                Console.WriteLine("This is a super message");
+            }
+    }
+}
