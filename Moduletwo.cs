6969a49433aaa19eeb2daab2f5b5eb16f7345854
Zack Spring Chief <jzacksc@gmail.com>
2023-10-01 13:15:50 -0600

    adding arrays

--- Moduletwo.cs
+++ Moduletwo.cs
@@ -58,13 +58,48 @@ namespace SoloLearn
 	{
 		static void Main(string[] args)
 		{
+			int[ ] a = new int[10]; // creates array
+			for (int k = 0; k < 10; k++) {
+				a[k] = k*2; //fills in array
+			}
+			for (int k = 0; k < 10; k++) { //iterates array
+				Console.WriteLine(a[k]); // outputs array results
+			}
+		}
+	}
+}
+
+//foreach provides shorter and easier way of accessing an array elements
+namespace SoloLearn
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
 			int[ ] a = new int[10];
 			for (int k = 0; k < 10; k++) {
 				a[k] = k*2;
 			}
-			for (int k = 0; k < 10; k++) {
-				Console.WriteLine(a[k]);
+			foreach (int k in a) {
+				Console.WriteLine(k);
 			}
+		}
+	}
+}
+
+namespace SoloLearn
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[ ] arr = {11, 35, 62, 555, 989};
+			int sum = 0; 
+			
+			foreach (int x in arr) {
+				sum += x;
+			}
+			Console.WriteLine(sum);
 		}
 	}
 }
