50f34d879ff164fb9196585a4c690d71f4a73e32
Zack Spring Chief <jzacksc@gmail.com>
2023-09-30 20:20:30 -0600

    first instance of arrays

--- Moduletwo.cs
+++ Moduletwo.cs
@@ -1,1 +1,8 @@
 //arrays 
+int[]myArray; //to delcare array
+int[]myArray = new int[5]; //arrays are object and we need to instantiate them like so.
+myArray[0] = 23; //this would assign the first element of the array the value of 23.
+
+string[] names = {"zack", "dan", "kyle"};
+double[] prices = {23, 33, 55};
+//short hand of writing an array.
