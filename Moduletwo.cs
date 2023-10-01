083a02d80daa5537016325844a3fcd2522600557
Zack Spring Chief <jzacksc@gmail.com>
2023-10-01 12:27:10 -0600

    getting user input and outputing result

--- Moduletwo.cs
+++ Moduletwo.cs
@@ -6,3 +6,32 @@ myArray[0] = 23; //this would assign the first element of the array the value of
 string[] names = {"zack", "dan", "kyle"};
 double[] prices = {23, 33, 55};
 //short hand of writing an array.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] games = { "Alien Shooter", "Tic Tac Toe", "Snake", "Puzzle", "Football" };
+
+            //your code goes here
+               int x = Convert.ToInt32( Console.ReadLine());
+            if (x < 0 || x > 4)
+            {
+                Console.WriteLine("Invalid number");
+            }
+        
+            else
+            {
+            Console.WriteLine(games[x]);
+            
+        }
+    }
+}
