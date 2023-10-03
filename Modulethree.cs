d388edcdf08f0eb121af9aa7a220c5c1370b612e
Zack Spring Chief <jzacksc@gmail.com>
2023-10-03 13:10:15 -0600

    soing minor review in module 3

---
+++ Modulethree.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//delcare a string
+string name = "Zack";
+//declare a integer
+int age = 33; //no need for quotes and we can declare using int, double, and float(we use f for float)
+//declare booleon
+bool isCool = true;
+// delcare single letter 
+char grade = 'A';
+//how to access name varialbe 
+Console.WriteLine(name); //can use any variable
