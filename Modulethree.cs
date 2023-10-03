fbe311703b9143d3be5dc204536079118ba866af
Zack Spring Chief <jzacksc@gmail.com>
2023-10-03 13:16:48 -0600

    knowledge is power

--- Modulethree.cs
+++ Modulethree.cs
@@ -13,4 +13,17 @@ bool isCool = true;
 // delcare single letter 
 char grade = 'A';
 //how to access name varialbe 
-Console.WriteLine(name); //can use any variable
+Console.WriteLine(name); //can use any variable OR
+Console.Write(age); //this does not write in a line
+//user interactivity
+Console.Readline(); //gets user to input information
+//Converting
+Convert.ToInt32(); //can use other numbers like 16, 32, and 64
+Convert.ToString(); //convert input to string.
+// there are other methods
+name.ToUpper(); //converts name to uppercase in this example.
+name.ToLower(); //same but to lower case;
+name.Length; //gives length aomunt in value
+// how to store an user inputted value
+//declare value
+string input = Console.Readline();
