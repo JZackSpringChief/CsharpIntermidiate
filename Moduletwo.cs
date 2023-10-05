779ed710f8ce02024bc966166cfb1e3330d8d75b
Zack Spring Chief <jzacksc@gmail.com>
2023-10-04 19:13:27 -0600

    working with strings

--- Moduletwo.cs
+++ Moduletwo.cs
@@ -270,3 +270,33 @@ int[ ] arr = { 2, 4, 7, 1};
 		}
     }
 }
+
+//working with strings
+		{
+			string a = "some text";
+			Console.WriteLine(a.Length);
+			//Outputs 9
+
+			Console.WriteLine(a.IndexOf('t'));
+			//Outputs 5
+
+			 a = a.Insert(0, "This is ");
+			Console.WriteLine(a);
+			//Outputs "This is some text"
+
+			a = a.Replace("This is", "I am");
+			Console.WriteLine(a);
+			//Outputs "I am some text"
+
+			if(a.Contains("some"))
+				Console.WriteLine("found");
+			//Outputs "found"
+
+			a = a.Remove(4);
+			Console.WriteLine(a);
+			//Outputs "I am"
+
+			a = a.Substring(2);
+			Console.WriteLine(a);
+			//Outputs "am"
+		}
