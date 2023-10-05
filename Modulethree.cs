3e2d7ed4ef9eae6b98c9999b09bd997135adddd3
Zack Spring Chief <jzacksc@gmail.com>
2023-10-05 14:08:04 -0600

    fixed code

--- Modulethree.cs
+++ Modulethree.cs
@@ -314,3 +314,42 @@ class Person {
 
 readonly string name; // OK can be declared without initialization.
 const double PI; // Error as it must be initialized when it is declared
+
+//readonly can only be changed within its contructor. but a const cannot
+//readonly can be assigned a value that is the result of a calculation but const cannot. ex
+readonly double a = Math.Sin(60); // OK
+
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Avg avg = new Avg(num1, num2);
+
+            Console.WriteLine(avg.GetAvg());
+        }
+    }
+
+    class Avg
+    {
+        double num1;
+        double num2;
+        
+        //create the constructor
+        	public Avg(double num1, double num2)
+        	{
+        		this.num1 = num1;
+        		this.num2 = num2;
+        	}
+
+        public double GetAvg()
+        {
+            return (num1 + num2)/2;
+        }
+    }
+}
