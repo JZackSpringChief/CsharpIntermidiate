5b4a95812d87855c3bbc9965fafbdcdda9331346
Zack Spring Chief <jzacksc@gmail.com>
2023-10-04 22:06:24 -0600

    testing prettier to work with c#

---
+++ test.js
@@ -0,0 +1,17 @@
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(4, 1));
+
+let z = 9001;
+
+function overNineThousand() {
+  if (z > 9000) {
+    return "OVER 9000!!!!";
+  } else if (z < 9000) {
+    return "Git good son!";
+  } else {
+    console.log("not a valid input");
+  }
+}
+console.log(overNineThousand());
