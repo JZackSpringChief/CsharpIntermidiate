b6f38001f0b8460dbe330c8056f2709a4d2b8822
Zack Spring Chief <jzacksc@gmail.com>
2023-10-05 22:14:48 -0600

    got the questin right

--- Modulethree.cs
+++ Modulethree.cs
@@ -517,3 +517,45 @@ static void Main(string[] args) {
         		Score res = new Score(s, s2);
         		return res;
     }
+
+	namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            int points1 = Convert.ToInt32(Console.ReadLine());
+            string name2 = Console.ReadLine();
+            int points2 = Convert.ToInt32(Console.ReadLine());
+
+            DancerPoints dancer1 = new DancerPoints(name1, points1);
+            DancerPoints dancer2 = new DancerPoints(name2, points2);
+
+            DancerPoints total = dancer1 + dancer2;
+            Console.WriteLine(total.name);
+            Console.WriteLine(total.points);
+        }
+    }
+
+    class DancerPoints
+    {
+        public string name;
+        public int points;
+        public DancerPoints(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        //overload the + operator
+        	public static DancerPoints operator+(DancerPoints a, DancerPoints b){
+        		string z = a.name + " & " + b.name;
+        		int q = a.points + b.points;
+        		
+        		DancerPoints res = new DancerPoints(z, q);
+        		return res;
+        	}
+
+    }
+}
