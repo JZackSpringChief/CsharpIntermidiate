4647d309ecca07662b71ed95c8d5c21806a80ba9
Zack Spring Chief <jzacksc@gmail.com>
2023-10-05 22:16:34 -0600

    fixed errors and added chapter

--- Modulethree.cs
+++ Modulethree.cs
@@ -518,29 +518,12 @@ static void Main(string[] args) {
         		return res;
     }
 
-	namespace Code_Coach_Challenge
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string name1 = Console.ReadLine();
-            int points1 = Convert.ToInt32(Console.ReadLine());
-            string name2 = Console.ReadLine();
-            int points2 = Convert.ToInt32(Console.ReadLine());
 
-            DancerPoints dancer1 = new DancerPoints(name1, points1);
-            DancerPoints dancer2 = new DancerPoints(name2, points2);
 
-            DancerPoints total = dancer1 + dancer2;
-            Console.WriteLine(total.name);
-            Console.WriteLine(total.points);
-        }
-    }
 
-    class DancerPoints
-    {
-        public string name;
+
+ 
+public string name;
         public int points;
         public DancerPoints(string name, int points)
         {
@@ -557,5 +540,4 @@ static void Main(string[] args) {
         		return res;
         	}
 
-    }
-}
+    
