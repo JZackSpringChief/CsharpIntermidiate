14a0e3a649a6185466b75e72cb2d33770c009278
Zack Spring Chief <jzacksc@gmail.com>
2023-10-06 19:34:16 -0600

    oop is so cool

--- modulefour.cs
+++ modulefour.cs
@@ -62,3 +62,54 @@ class Dog : Animal { //derived
 			s.Speak();
 		}
 	}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string brandName = Console.ReadLine();
+            string modelName = Console.ReadLine();
+
+            Car car = new Car();
+            car.Brand = brandName;
+            car.Model = modelName;
+
+            car.ShowBrand();
+            car.ShowModel();
+            
+            	  	/*Car a = new Car();
+        	a.ShowBrand();
+        	a.ShowModel();*/ //need to delete but will keep in as i messed this up.
+        }
+    }
+    class Vehicle
+    {
+        public string Brand { get; set; }
+
+        public void ShowBrand()
+        {
+            Console.WriteLine("Brand: " + Brand);
+        }
+    }
+    
+    //complete the Car class
+    class Car : Vehicle
+    {
+        public string Model { get; set; }
+        
+        public void ShowModel()
+        {
+            Console.WriteLine("Model: " + Model);
+        }
+        
+    }
+}
