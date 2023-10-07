3eedecfdbf30933cd534770dac473b117749e709
Zack Spring Chief <jzacksc@gmail.com>
2023-10-06 20:12:49 -0600

    playground

--- modulefour.cs
+++ modulefour.cs
@@ -336,3 +336,53 @@ class Rectangle : Shape {
     Console.WriteLine("Rect Draw");
   }
 }
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+	class Program
+	{
+		class Shape {
+			public virtual void Draw() {
+				Console.WriteLine("Base Draw");
+			}
+		}
+		class Circle : Shape {
+			public override void Draw() {
+				// draw a circle...
+				Console.WriteLine("Circle Draw");
+			}
+		}
+		class Rectangle : Shape {
+			public override void Draw() {
+				// draw a rectangle...
+				Console.WriteLine("Rectangle Draw");
+			}
+		}
+		class Square : Shape {
+		    public override void Draw() {
+		        // draw a square...
+		        Console.WriteLine("Square Draw");
+		    }
+		}
+		static void Main(string[] args)
+		{
+			Shape c = new Circle();
+			c.Draw();
+
+			Shape r = new Rectangle();
+			r.Draw();
+			
+			Shape s = new Square();
+			
+			Shape z = new Shape();
+			z.Draw();
+			s.Draw();
+		}
+	}
+}
