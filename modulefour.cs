bda2786b81d604a2c119297973f932389f526717
Zack Spring Chief <jzacksc@gmail.com>
2023-10-06 20:21:01 -0600

    passed test

--- modulefour.cs
+++ modulefour.cs
@@ -386,3 +386,49 @@ namespace SoloLearn
 		}
 	}
 }
+
+// test q
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit unit1 = new Unit();
+            Unit musketeer = new Musketeer();
+            Unit magician = new Magician();
+
+            unit1.Attack();
+            musketeer.Attack();
+            magician.Attack();
+        }
+    }
+
+    class Unit
+    {
+        public virtual void Attack()
+        {
+            Console.WriteLine("Using sword!");
+        }
+    }
+    
+    /*derive the class from Unit class
+    and override Attack() method*/
+    class Musketeer : Unit
+    {
+        public override void Attack()
+        {
+            Console.WriteLine("Musketeer ATTACK!");
+        }
+    }
+    /*derive the class from Unit class
+    and override Attack() method*/
+    class Magician : Unit
+    {
+        public override void Attack()
+        {
+            Console.WriteLine("Magician ATTACK!");
+        }
+    }
+}
