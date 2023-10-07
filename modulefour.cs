fa57fd9c2abe772f4bae7a7061a1fb849330e66e
Zack Spring Chief <jzacksc@gmail.com>
2023-10-06 19:51:27 -0600

    completed the test

--- modulefour.cs
+++ modulefour.cs
@@ -176,3 +176,43 @@ namespace SoloLearn
 			
 		}
 	}
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string accNumber = Console.ReadLine();
+            double balance = Convert.ToDouble(Console.ReadLine());
+
+            User user = new User(accNumber, balance);
+
+            user.ShowDetails();
+
+        }
+    }
+
+    class Account
+    {
+        /*private*/ protected double Balance { get; set; }
+    }
+
+    class User : Account
+    {
+        protected string AccNumber { get; set; }
+        
+        //complete the constructor
+        public User(string accNumber, double balance)
+        {
+            AccNumber = accNumber;
+            Balance = balance;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Account N: " + AccNumber);
+            Console.WriteLine("Balance: " + Balance);
+        }
+    }
+}
