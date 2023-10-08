f60824635bf23580aabfe4e8dd934866bdc259d4
Zack Spring Chief <jzacksc@gmail.com>
2023-10-07 21:19:17 -0600

    passed test question

--- modulefour.cs
+++ modulefour.cs
@@ -660,3 +660,64 @@ namespace SoloLearn {
   }
 }
 //the .NET frame work uses namespaces to organize its many classes.
+//drawing application
+
+using System;
+using System.Collections.Generic;
+
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Draw pencil = new Draw();
+            Draw brush = new Brush();
+            Draw spray = new Spray();
+
+            pencil.StartDraw();
+            brush.StartDraw();
+            spray.StartDraw();
+
+        }
+    }
+
+    /*
+    Draw => "Using pencil"
+    Brush => "Using brush"
+    Spray => "Using spray"
+    */
+
+    public interface IDraw
+    {
+        void StartDraw();
+    }
+
+    class Draw : IDraw
+    {
+        public virtual void StartDraw()
+        {
+            Console.WriteLine("Using pencil");
+        }
+    }
+
+    //inherit this class from the class Draw
+    class Brush : Draw 
+    {
+        //implement the StartDraw() method
+        	public override void StartDraw() {
+        		Console.WriteLine("Using brush");
+        	}
+
+    }
+
+    //inherit this class from the class Draw
+    class Spray : Draw
+    {
+        //implement the StartDraw() method
+        	public override void StartDraw() {
+        		Console.WriteLine("Using spray");
+        	}
+
+    }
+}
