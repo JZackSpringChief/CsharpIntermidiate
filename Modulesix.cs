67180057eeb9caf0a18e18d016f2699cc386bfde
Zack Spring Chief <jzacksc@gmail.com>
2023-10-09 00:07:53 -0600

    test failed

--- Modulesix.cs
+++ Modulesix.cs
@@ -410,3 +410,39 @@ namespace SoloLearn
 		}
 	}
 }
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> candidates = new HashSet<string>();
+
+            candidates.Add("John");
+            candidates.Add("Amelie");
+            candidates.Add("Tom");
+            candidates.Add("Richard");
+            candidates.Add("Barbara");
+            candidates.Add("Susan");
+            candidates.Add("Charles");
+            candidates.Add("Daniel");
+            candidates.Add("Tamara");
+            candidates.Add("Donald");
+
+            HashSet<string> hiring = new HashSet<string>();
+
+            while (hiring.Count<3)
+            {
+                string hire = Console.ReadLine();
+                hiring.Add(hire);
+            }
+            Console.Write(hiring.IsSubsetOf(candidates) ? "Starting hiring process" : "Something is wrong");
+        }
+    }
+}
