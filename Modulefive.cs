3dfa9b39c4856070ceaae246fe892b3e8bbd19d0
Zack Spring Chief <jzacksc@gmail.com>
2023-10-08 21:32:19 -0600

    structs

--- Modulefive.cs
+++ Modulefive.cs
@@ -1,1 +1,7 @@
-//struts
+//struts - a strut is a type of value used to encapsulate small groups of related variables such as
+// the coordinates of a rectangle or the characteristics of an item in an inventory. ex
+struct Book {
+  public string title;  
+  public double price;
+  public string author;
+}
