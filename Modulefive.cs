62aa1489251e5f0b4e6dcf171e90891301e2cf62
Zack Spring Chief <jzacksc@gmail.com>
2023-10-08 21:36:37 -0600

    more notes

--- Modulefive.cs
+++ Modulefive.cs
@@ -5,3 +5,38 @@ struct Book {
   public double price;
   public string author;
 }
+		static void Main(string[] args)
+		{
+			Book b;
+			b.title = "Test";
+			b.price = 5.99;
+			b.author = "David";
+			
+			Console.WriteLine(b.title);
+		}
+
+//structs share most of the syntax of classes, but are more limited than classes.
+//unlike classes, structs can be instantiated without using a new operator
+//structs do not support inheritance and cannot contain virtual methods.
+//Structs can contain methods, properties, indexers, and so on. Structs cannot contain default constructors 
+//(a constructor without parameters), but they can have constructors that take parameters. 
+//In that case the new keyword is used to instantiate a struct object, similar to class objects. ex
+namespace SoloLearn
+{
+	class Program
+	{
+		struct Point {
+			public int x;
+			public int y;
+			public Point(int x, int y) {
+				this.x = x;
+				this.y = y;
+			}
+		}
+		static void Main(string[] args)
+		{
+			Point p = new Point(10, 15);
+			Console.WriteLine(p.x);
+		}
+	}
+}
