c2a16ade1f720a3721591ec8e402da6862c8ec0d
Zack Spring Chief <jzacksc@gmail.com>
2023-10-08 21:45:07 -0600

    passes test

--- Modulefive.cs
+++ Modulefive.cs
@@ -40,3 +40,52 @@ namespace SoloLearn
 		}
 	}
 }
+//structs are much more simpler than classes. consider using a struct when trying to represent a simple set of data.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = Convert.ToInt32(Console.ReadLine());
+            int width = Convert.ToInt32(Console.ReadLine());
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Cuboid cuboid = new Cuboid(length, width, height);
+
+            Console.WriteLine("Volume: " + cuboid.Volume());
+            Console.WriteLine("Perimeter: " + cuboid.Perimeter());
+        }
+    }
+    struct Cuboid
+    {
+        public int length;
+        public int width;
+        public int height;
+
+        //create a constructor
+        	public Cuboid(int length, int width, int height){
+        		this.length = length;
+        		this.width = width; 
+        		this.height = height;
+        	}
+        
+        
+        //complete this method
+        public int Volume()
+        {
+            	return length*width*height;
+        }
+        //complete this method
+        public int Perimeter()
+        {
+            	return 4*(length+width+height);
+        }
+    }
+}
